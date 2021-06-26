5764d70f0d8138dc303e1fd56eaf72c6697074a2
Wagner Oliveira <41020936+Wagner85@users.noreply.github.com>
2021-06-26 17:04:05 -0300

    Update Python_Script.py

--- Python_Script.py
+++ Python_Script.py
@@ -1,32 +1,22 @@
 import os
-import tempfile
-
 import boto3
-from PIL import Image
 
-s3 = boto3.client('s3')
-DEST_BUCKET = os.environ['DEST_BUCKET']
-SIZE = 128, 128
+AMI = os.environ['AMI']
+INSTANCE_TYPE = os.environ['INSTANCE_TYPE']
+KEY_NAME = os.environ['KEY_NAME']
+SUBNET_ID = os.environ['SUBNET_ID']
 
+ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
+    instance = ec2.create_instances(
+        ImageId=AMI,
+        InstanceType=INSTANCE_TYPE,
+        KeyName=KEY_NAME,
+        SubnetId=SUBNET_ID,
+        MaxCount=1,
+        MinCount=1
 
-    for record in event['Records']:
-        source_bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        thumb = 'thumb-' + key
-        with tempfile.TemporaryDirectory() as tmpdir:
-            download_path = os.path.join(tmpdir, key)
-            upload_path = os.path.join(tmpdir, thumb)
-            s3.download_file(source_bucket, key, download_path)
-            generate_thumbnail(download_path, upload_path)
-            s3.upload_file(upload_path, DEST_BUCKET, thumb)
+    )
 
-        print('Thumbnail image saved at {}/{}'.format(DEST_BUCKET, thumb))
-
-
-def generate_thumbnail(source_path, dest_path):
-    print('Generating thumbnail from:', source_path)
-    with Image.open(source_path) as image:
-        image.thumbnail(SIZE)
-        image.save(dest_path)
+    print("New instance created:", instance[0].id)
